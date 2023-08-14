c9b99f0f40e24284c8f9190e3f63b9b5a8dafb16
Michael Rose <michael@rosenetwork.net>
2023-08-14 16:43:58 -0700

    basic views implimentation

--- notes_app/views.py
+++ notes_app/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from .models import Notes
 
 # Create your views here.
+
+
+class NotesList(ListView):
+    model = Notes
+    fields = "__all__"
+
+
+class NotesDetail(DetailView):
+    model = Notes
+
+
+class NoteCreate(CreateView):
+    model = Notes
+    fields = "__all__"
+    success_url = "/notes"
+
+
+class NoteUpdate(UpdateView):
+    model = Notes
+    fields = "__all__"
+
+
+class NoteDelete(DeleteView):
+    model = Notes
+    success_url = "/notes"
